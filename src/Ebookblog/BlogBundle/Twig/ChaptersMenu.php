5b6040a4dd0fe178b76e48244e6d1d061603cc12
Melanie Bousquet <bousquet.melanie@gmail.com>
2017-04-30 21:04:28 +0200

    Modif extension Twig = création d'une fonction Twig / Suppression variables globales

--- src/Ebookblog/BlogBundle/Twig/ChaptersMenu.php
+++ src/Ebookblog/BlogBundle/Twig/ChaptersMenu.php
@@ -1,21 +1,33 @@
 <?php
 
+/* Extension Twig : Dynamic Menu of the publicated Chapters, for all 'Front' Views */
+
 namespace Ebookblog\BlogBundle\Twig;
 
-
+use Twig_Environment as Environment;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 
-class ChaptersMenu extends \Twig_Extension implements \Twig_Extension_GlobalsInterface
+class ChaptersMenu extends \Twig_Extension
 {
     protected $doctrine;
+    protected $twig;
 
-    public function __construct(RegistryInterface $doctrine)
+    public function __construct(RegistryInterface $doctrine, Environment $twig)
     {
         $this->doctrine = $doctrine;
+        $this->twig = $twig;
     }
 
-    public function getGlobals ()
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('getChapters', array($this, 'getChapters'))
+        );
+
+    }
+
+    public function getChapters()
     {
         $repository = $this->doctrine
             ->getManager()
@@ -25,9 +37,14 @@ class ChaptersMenu extends \Twig_Extension implements \Twig_Extension_GlobalsInt
                     array('published' => 1)
             );
 
-        return array(
+         return $this->twig->render('front/menu.html.twig', array (
             'listChapters' => $listChapters
-        );
 
+        ));
+    }
+
+    public function getName()
+    {
+        return 'ChaptersMenu';
     }
 }
